9924e56bb37ea0ade875454324a580114c010f97
Marco Tulio <marcotuliocandeo@outlook.com>
2024-02-23 17:49:03 -0300

    migrate nvim config to lua files

---
+++ lua/core/plugin_config/init.lua
@@ -0,0 +1,7 @@
+require("core.plugin_config.catppuccin")
+require("core.plugin_config.lualine")
+require("core.plugin_config.nvim-tree")
+require("core.plugin_config.telescope")
+require("core.plugin_config.treesitter")
+require("core.plugin_config.lsp_config")
+
