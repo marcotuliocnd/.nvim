71f3fa02b90ba45907c32c14118722b9be28cd11
Marco Túlio Candeo <marcotuliocandeo@outlook.com>
2024-02-28 15:10:02 -0300

    configure eslint

--- lua/core/plugin_config/lsp_config.lua
+++ lua/core/plugin_config/lsp_config.lua
@@ -6,6 +6,14 @@ require("mason-lspconfig").setup({
 
 local lspconfig = require('lspconfig')
 lspconfig.tsserver.setup({})
+lspconfig.eslint.setup({
+  on_attach = function(client, bufnr)
+    vim.api.nvim_create_autocmd("BufWritePre", {
+      buffer = bufnr,
+      command = "EslintFixAll",
+    })
+  end,
+})
 
 vim.api.nvim_create_autocmd('LspAttach', {
   group = vim.api.nvim_create_augroup('UserLspConfig', {}),
