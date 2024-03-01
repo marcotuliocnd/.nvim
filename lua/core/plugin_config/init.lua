7c69225ed7e3615690633c6fc73cd8c9618be2f7
Marco Túlio Candeo <marcotuliocandeo@outlook.com>
2024-03-01 13:54:31 -0300

    updated color theme

--- lua/core/plugin_config/init.lua
+++ lua/core/plugin_config/init.lua
@@ -1,4 +1,4 @@
-require("core.plugin_config.catppuccin")
+require("core.plugin_config.everforest")
 require("core.plugin_config.lualine")
 require("core.plugin_config.nvim-tree")
 require("core.plugin_config.telescope")
